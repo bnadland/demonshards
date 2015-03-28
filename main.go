7ceee4aa56ae1c163806fa9fd595d5f659a7cf22
Benjamin Nadland <me@benjaminnadland.de>
2015-03-28 14:33:49 +0100

    add some basic controls

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/nsf/termbox-go"
+	"strconv"
 	"unicode/utf8"
 )
 
 type Viking struct {
-	Sprite rune
-	X      int
-	Y      int
+	Sprite     rune
+	Color      termbox.Attribute
+	Background termbox.Attribute
+	X          int
+	Y          int
 }
 
 func NewViking(sprite string, x int, y int) *Viking {
 	spriteRune, _ := utf8.DecodeRuneInString(sprite)
 	return &Viking{
-		Sprite: spriteRune,
-		X:      x,
-		Y:      y,
+		Sprite:     spriteRune,
+		Color:      termbox.ColorGreen,
+		Background: termbox.ColorBlack,
+		X:          x,
+		Y:          y,
+	}
+}
+
+func render(text string, x int, y int) {
+	for i, textRune := range text {
+		termbox.SetCell(x+i, y, textRune, termbox.ColorWhite, termbox.ColorBlack)
 	}
 }
 
@@ -45,31 +57,65 @@ func main() {
 
 	running := true
 
+	tiles := make(map[string]rune)
+
+	for _, tile := range []string{".", "#"} {
+		tileRune, _ := utf8.DecodeRuneInString(tile)
+		tiles[tile] = tileRune
+	}
+
+	selectedViking := 1
+
 	for running {
-		for _, viking := range vikings {
-			termbox.SetCell(viking.X, viking.Y, viking.Sprite, termbox.ColorGreen, termbox.ColorBlack)
+
+		render(fmt.Sprintf("[%s] Viking", strconv.Itoa(selectedViking)), 0, 0)
+
+		for x := 0; x < 80; x++ {
+			for y := 1; y < 20; y++ {
+				termbox.SetCell(x, y, tiles["."], termbox.ColorWhite, termbox.ColorBlack)
+			}
 		}
+
+		for _, viking := range vikings {
+			termbox.SetCell(viking.X, viking.Y, viking.Sprite, viking.Color, viking.Background)
+		}
+
 		termbox.SetCursor(cursor.X, cursor.Y)
+
 		termbox.Flush()
 
 		event := termbox.PollEvent()
 		if event.Type == termbox.EventKey {
-			switch event.Key {
-			case termbox.KeyEsc:
+			if event.Key == termbox.KeyEsc || event.Ch == 'q' {
 				running = false
-				break
-			case termbox.KeyArrowUp:
+			}
+			if event.Key == termbox.KeyArrowUp || event.Ch == 'k' {
 				cursor.Y -= 1
-				break
-			case termbox.KeyArrowDown:
+			}
+			if event.Key == termbox.KeyArrowDown || event.Ch == 'j' {
 				cursor.Y += 1
-				break
-			case termbox.KeyArrowLeft:
+			}
+			if event.Key == termbox.KeyArrowLeft || event.Ch == 'h' {
 				cursor.X -= 1
-				break
-			case termbox.KeyArrowRight:
+			}
+			if event.Key == termbox.KeyArrowRight || event.Ch == 'l' {
 				cursor.X += 1
-				break
+			}
+
+			if event.Ch == '1' {
+				selectedViking = 1
+				cursor.X = vikings[0].X
+				cursor.Y = vikings[0].Y
+			}
+			if event.Ch == '2' {
+				selectedViking = 2
+				cursor.X = vikings[1].X
+				cursor.Y = vikings[1].Y
+			}
+			if event.Ch == '3' {
+				selectedViking = 3
+				cursor.X = vikings[2].X
+				cursor.Y = vikings[2].Y
 			}
 		}
 	}
